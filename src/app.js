544b88fc73b5e88ff5058d1ca709c859dfb3ae3c
Jean Garzón <85135998+jeangq24@users.noreply.github.com>
2024-07-28 16:58:21 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const routes = require('./routes/index.js');
 const { morganLogging, bodyParserUrlencoded, bodyParseJson, configCors, errorHandler, notFoundHandler, swaggerConfig } = require('./middleware/configServer.js');
-const path = require("path")
 
 //Init
 const server = express();
@@ -16,7 +15,6 @@ server.use(
   configCors,
 );
 
-
 //Routes
 server.use(
   '/',
@@ -24,23 +22,17 @@ server.use(
 );
 
 //Swagger route 
-const __swaggerDistPath = path.join(
-  __dirname,
-  'node_modules',
-  'swagger-ui-dist'
-);
-
 const [swaggerEndpoint, swaggerServe, swaggerSetup] = swaggerConfig();
-
-server.use(swaggerEndpoint,
-  express.static(__swaggerDistPath, { index: false }), 
+server.use(
+  swaggerEndpoint,
   swaggerServe,
   swaggerSetup
 );
+
 //Handlers
 server.use(
-  notFoundHandler,
   errorHandler,
+  notFoundHandler,
 );
 
 //Modules exports
